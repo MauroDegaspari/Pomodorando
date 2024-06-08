55a9535b679520588ea987baf57c385f5671b7d3
MauroDegaspari <mauro_degaspari@hotmail.com>
2024-06-08 00:47:31 -0300

    Javascript - Ajuste em imagens

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const html = document.querySelector('html')
 const focobtn = document.querySelector('.app__card-button--foco')
 const curtobtn = document.querySelector('.app__card-button--curto')
 const logobtn = document.querySelector('.app__card-button--longo') 
+const image = document.querySelector('.app__image')
 
 focobtn.addEventListener('click', () =>{
     html.setAttribute('data-contexto', 'foco')
+    image.setAttribute('src','imagens/foco.png')
 })
 
 curtobtn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    image.setAttribute('src', 'imagens/descanso-curto.png')
 })
 
 logobtn.addEventListener('click', () =>{
    html.setAttribute('data-contexto','descanso-longo') 
+   image.setAttribute('src','imagens/descanso-longo.png')
 })
 
